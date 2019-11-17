365ac4f649f8516190780d2c40142d16b4aec139
Sadat Jubayer <sadatjubayer@gmail.com>
2019-11-17 23:54:02 +0600

    test content

---
+++ src/components/search-box/card-test/Card.jsx
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// import "./style.css";
+
+export default function Card(props) {
+  return (
+    <div className="card-container">
+      <img
+        // src={`https://robohash.org/${props.monster.id}?set=set2&size=180*180`}
+        src={`https://api.adorable.io/avatars/280/${props.monster.id}.png`}
+        alt="monster"
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+}
